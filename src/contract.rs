d7d567b5f07adc7e23a8b0089bbab6382af8c619
Joshua Brown <jbr242@uclive.ac.nz>
2024-10-08 14:28:02 +1300

    Completed the contract

--- src/contract.rs
+++ src/contract.rs
@@ -1,314 +1,211 @@
-use cosmwasm_std::{
-    entry_point, to_binary, Addr, BankMsg, Binary, BlockInfo, CanonicalAddr, Coin, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult, Storage, Timestamp, Uint128, Uint64
-};
-use secret_toolkit::permit::Permit;
 
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryAnswer, QueryMsg, QueryWithPermit};
-use crate::state::{add_to_bid, Auction, HighestBid, ADMIN, AUCTION, AUCTION_STARTED, BIDS, HIGHEST_BID, SALE_COMPLETED};
+use cosmwasm_std::{
+    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError,
+    StdResult, Storage,
+};
+
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryAnswer, QueryMsg};
+use crate::state::{Pet, PET, PASSWORD, OWNER};
+
+const BLOCK_CONVERSION: f64 = 0.1;
 
 #[entry_point]
 pub fn instantiate(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
-    let admin = msg.admin.unwrap_or(info.sender);
-    let admin = deps.api.addr_canonicalize(admin.as_str())?;
-    ADMIN.save(deps.storage, &admin)?;
+    let owner = msg.owner.unwrap_or(info.sender);
+    let owner = deps.api.addr_canonicalize(owner.as_str())?;
+    OWNER.save(deps.storage, &owner)?;
 
-    AUCTION_STARTED.save(deps.storage, &false)?;
-    SALE_COMPLETED.save(deps.storage, &false)?;
+    if msg.name.len() < 1 {
+        return Err(StdError::generic_err("Name cannot be empty"));
+    }
 
+    let pet = Pet {
+        name: msg.name,
+        hunger_level: 0,
+        happiness_level: 10,
+        energy_level: 10,
+        last_action_block: env.block.height,
+    };
+
+    PET.save(deps.storage, &pet)?;
     Ok(Response::default())
 }
 
 #[entry_point]
-pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
-    match msg {
-        ExecuteMsg::SetAuction { secret, minimum_bid, end_time } => try_set_auction(
-            deps, 
-            env, 
-            info.sender, 
-            secret, 
-            minimum_bid, 
-            end_time
-        ),
-        ExecuteMsg::StartAuction { } => try_start_auction(deps, info.sender),
-        ExecuteMsg::Bid { } => try_bid(deps, env, info),
-        ExecuteMsg::Withdraw { } => try_withdraw(deps, env, info.sender),
-    }
-}
-
-// pub fn try_increment(deps: DepsMut, _env: Env) -> StdResult<Response> {
-//     CONFIG.update(deps.storage, |mut state| {
-//         state.count += 1;
-//         Ok(state)
-//     })?;
-
-//     Ok(Response::default())
-// }
-
-// pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> StdResult<Response> {
-//     let sender_address = info.sender.clone();
-//     CONFIG.update(deps.storage, |mut state| {
-//         if sender_address != state.owner {
-//             return Err(StdError::generic_err("Only the owner can reset count"));
-//         }
-//         state.count = count;
-//         Ok(state)
-//     })?;
-
-//     Ok(Response::default())
-// }
-
-// #[entry_point]
-// pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-//     match msg {
-//         QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
-//     }
-// }
-
-// fn query_count(deps: Deps) -> StdResult<CountResponse> {
-//     let state = CONFIG.load(deps.storage)?;
-//     Ok(CountResponse { count: state.count })
-// }
-
-pub fn try_set_auction(
-    deps: DepsMut, 
-    _env: Env, 
-    sender: Addr, 
-    secret: String, 
-    minimum_bid: Uint128, 
-    end_time: Uint64
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> StdResult<Response> {
-    // check if sender is admin
-    let admin = deps.api.addr_humanize(&ADMIN.load(deps.storage)?)?;
-    if sender != admin {
-        return Err(StdError::generic_err("Unauthorized"));
+    match msg {
+        ExecuteMsg::SetPassword { password } => try_set_password(deps, info, password),
+        ExecuteMsg::Feed { amount } => try_feed(deps, env, info, amount),
+        ExecuteMsg::Play { amount } => try_play(deps, env, info, amount),
+        ExecuteMsg::Rest { amount } => try_rest(deps, env, info, amount),
+        ExecuteMsg::Transfer { new_owner } => try_transfer(deps, info, new_owner),
     }
+}
 
-    // make sure the auction has not started
-    if AUCTION_STARTED.load(deps.storage)? {
-        return Err(StdError::generic_err("Auction has already started"));
+pub fn try_set_password(deps: DepsMut, info: MessageInfo, password: String) -> StdResult<Response> {
+    let owner = deps.api.addr_humanize(&OWNER.load(deps.storage)?)?;
+    if info.sender != owner {
+        return Err(StdError::generic_err("Unauthorised"));
     }
-
-    let end_time = Timestamp::from_seconds(end_time.u64());
-    let minimum_bid = minimum_bid.u128();
-    AUCTION.save(deps.storage, &Auction{
-        secret,
-        end_time,
-        minimum_bid,
-    })?;
+    PASSWORD.save(deps.storage, &password)?;
 
     Ok(Response::default())
 }
 
-pub fn try_start_auction(deps: DepsMut, sender: Addr) -> StdResult<Response> {
-    // check if sender is admin
-    let admin = deps.api.addr_humanize(&ADMIN.load(deps.storage)?)?;
-    if sender != admin {
-        return Err(StdError::generic_err("Unauthorized"));
-    }
+fn update_state(storage: &mut dyn Storage, env: Env) -> StdResult<Pet> {
+    let mut pet = PET.load(storage)?;
 
-    // make sure the auction was not already started
-    if AUCTION_STARTED.load(deps.storage)? {
-        return Err(StdError::generic_err("Auction has already started"));
-    }
+    let mut blocks_passed = env.block.height - pet.last_action_block;
+    blocks_passed = (blocks_passed as f64 * BLOCK_CONVERSION) as u64;
+    //convert blocks_passed to be max 10
+    blocks_passed = blocks_passed.min(10);
 
-    // make sure the auction is set
-    if AUCTION.may_load(deps.storage)?.is_none() {
-        return Err(StdError::generic_err("Auction has not been set"));
-    }
-
-    AUCTION_STARTED.save(deps.storage, &true)?;
-
-    Ok(Response::default())
-}
-
-pub fn try_bid(deps: DepsMut, env: Env, info: MessageInfo) -> StdResult<Response> {
-    // make sure the auction has started
-    if !AUCTION_STARTED.load(deps.storage)? {
-        return Err(StdError::generic_err("Auction has not started"));
-    }
-
-    // make sure auction has not finished
-    if env.block.time > AUCTION.load(deps.storage)?.end_time {
-        return Err(StdError::generic_err("Auction has ended"));
-    }
-
-    // make sure some funds were sent
-    if info.funds.len() < 1 {
-        return Err(StdError::generic_err("No funds sent"));
-    }
-
-    // make sure the funds sent were SCRT (`uscrt` stands for micro-SCRT)
-    if info.funds[0].denom != "uscrt" {
-        return Err(StdError::generic_err("Bid not SCRT"));
-    }
-
-    let sender_address = deps.api.addr_canonicalize(info.sender.as_str())?;
-
-    // make sure the bidder is not the admin
-    if sender_address == ADMIN.load(deps.storage)? {
-        return Err(StdError::generic_err("Admin cannot bid"));
-    }
     
-    // add the sent funds to the bidder
-    let total_bid = add_to_bid(deps.storage, &sender_address, info.funds[0].amount.u128())?;
-
-    // check if this is the highest bid and update if so
-    if let Some(highest_bid) = HIGHEST_BID.may_load(deps.storage)? {
-        if total_bid > highest_bid.amount {
-            HIGHEST_BID.save(deps.storage, &HighestBid {
-                bidder: sender_address,
-                amount: total_bid,
-            })?;
-        }
-    } else { // no highest bid, yet
-        HIGHEST_BID.save(deps.storage, &HighestBid {
-            bidder: sender_address,
-            amount: total_bid,
-        })?;
-    }
+    // if hunger level above 10, make it 10 with min func
+    pet.hunger_level = (pet.hunger_level + blocks_passed as u8).min(10);
     
-    Ok(Response::default())
-}
-
-fn verify_auction_finished(
-    storage: &dyn Storage,
-    block: &BlockInfo,
-) -> StdResult<()> {
-    // make sure the auction has started
-    if !AUCTION_STARTED.load(storage)? {
-        return Err(StdError::generic_err("Auction has not started"));
-    }
-
-    // make sure the auction has finished
-    if block.time <= AUCTION.load(storage)?.end_time {
-        return Err(StdError::generic_err("Auction has not finished"));
-    }
-
-    Ok(())
-}
-
-pub fn try_withdraw(deps: DepsMut, env: Env, sender: Addr) -> StdResult<Response> {
-    verify_auction_finished(deps.storage, &env.block)?;
-
-    let message;
-
-    // withdraw completes sale for admin or withdraws failed bid for others (except winner)
-    let admin = deps.api.addr_humanize(&ADMIN.load(deps.storage)?)?;
-    let highest_bid = HIGHEST_BID.load(deps.storage)?;
-    if sender == admin {
-        if SALE_COMPLETED.load(deps.storage)? {
-            return Err(StdError::generic_err("Already completed sale"));
-        }
-        
-        let auction = AUCTION.load(deps.storage)?;
-        if highest_bid.amount < auction.minimum_bid {
-            return Err(StdError::generic_err("Auction ended without minimum bid reached"));
-        }
-
-        // send highest bid coins to the admin
-        let coins_to_send: Vec<Coin> = vec![Coin {
-            denom: "uscrt".to_string(),
-            amount: Uint128::from(highest_bid.amount),
-        }];
-        
-        message = CosmosMsg::Bank(BankMsg::Send {
-            to_address: sender.clone().into_string(),
-            amount: coins_to_send,
-        });
-
-        SALE_COMPLETED.save(deps.storage, &true)?;
-    } else if sender == deps.api.addr_humanize(&highest_bid.bidder)? {
-        return Err(StdError::generic_err("You won the auction, cannot withdraw"));
+    // if reducing happiness level would make it negative, make it 0
+    if pet.happiness_level >= blocks_passed as u8 {
+        pet.happiness_level -= blocks_passed as u8;
     } else {
-        let sender_canonical = deps.api.addr_canonicalize(sender.as_str())?;
-        let bid = BIDS.get(deps.storage, &sender_canonical).unwrap_or_default();
-        if bid == 0 {
-            return Err(StdError::generic_err("Nothing to withdraw"));
-        }
-
-        let coins_to_send: Vec<Coin> = vec![Coin {
-            denom: "uscrt".to_string(),
-            amount: Uint128::from(bid),
-        }];
-        
-        message = CosmosMsg::Bank(BankMsg::Send {
-            to_address: sender.clone().into_string(),
-            amount: coins_to_send,
-        });
-
-        BIDS.remove(deps.storage, &sender_canonical)?;
+        pet.happiness_level = 0;
     }
 
-    let res = Response::new().add_message(message);
-    Ok(res)
+    if pet.energy_level > 10 {
+        pet.energy_level = 10;
+    }
+
+    pet.last_action_block = env.block.height;
+
+    PET.save(storage, &pet)?;
+
+    Ok(pet)
 }
+
+
+
+pub fn try_feed(deps: DepsMut, env: Env, info: MessageInfo, amount: u8) -> StdResult<Response> {
+    let owner = deps.api.addr_humanize(&OWNER.load(deps.storage)?)?;
+    if info.sender != owner {
+        return Err(StdError::generic_err("Unauthorised"));
+    }
+
+    if amount > 10 {
+        return Err(StdError::generic_err("Amount must be between 0 and 10"));
+    }
+
+    let mut pet = update_state(deps.storage, env).unwrap();
+    pet.hunger_level = (pet.hunger_level + amount).min(10);
+    PET.save(deps.storage, &pet)?;
+
+    Ok(Response::default())
+}
+
+pub fn try_play(deps: DepsMut, env: Env, info: MessageInfo, amount: u8) -> StdResult<Response> {
+    let owner = deps.api.addr_humanize(&OWNER.load(deps.storage)?)?;
+    if info.sender != owner {
+        return Err(StdError::generic_err("Unauthorised"));
+    }
+
+    //Increase happiness_level by amount (ensure it doesn’t exceed 10).
+    if amount > 10 {
+        return Err(StdError::generic_err("Amount must be between 0 and 10"));
+    }
+
+    let mut pet = update_state(deps.storage, env).unwrap();
+    //Decrease energy_level by 1 (ensure it doesn’t go below 0).
+    if pet.energy_level < 1 {
+        return Err(StdError::generic_err("Not enough energy"));
+    }
+    pet.happiness_level = (pet.happiness_level + amount).min(10);
+    pet.energy_level -= 1;
+    PET.save(deps.storage, &pet)?;
+
+    Ok(Response::default())
+}
+
+pub fn try_rest(deps: DepsMut, env: Env, info: MessageInfo, amount: u8) -> StdResult<Response> {
+    let owner = deps.api.addr_humanize(&OWNER.load(deps.storage)?)?;
+    if info.sender != owner {
+        return Err(StdError::generic_err("Unauthorised"));
+    }
+
+    //Increase energy_level by amount (ensure it doesn’t exceed 10).
+    if amount > 10 {
+        return Err(StdError::generic_err("Amount must be between 0 and 10"));
+    }
+
+    let mut pet = update_state(deps.storage, env).unwrap();
+    pet.energy_level = (pet.energy_level + amount).min(10);
+    PET.save(deps.storage, &pet)?;
+
+    Ok(Response::default())
+}
+
+pub fn try_transfer(deps: DepsMut, info: MessageInfo, new_owner: String) -> StdResult<Response> {
+    let owner = deps.api.addr_humanize(&OWNER.load(deps.storage)?)?;
+    if info.sender != owner {
+        return Err(StdError::generic_err("Unauthorised"));
+    }
+
+    let new_owner = deps.api.addr_validate(&new_owner)?;
+    let new_owner = deps.api.addr_canonicalize(new_owner.as_str())?;
+    OWNER.save(deps.storage, &new_owner)?;
+
+    Ok(Response::default())
+}
+
 
 #[entry_point]
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::AuctionInfo{ } => query_auction_info(deps),
-        QueryMsg::WithPermit { permit, query } => permit_queries(deps, env, permit, query),
+        QueryMsg::IsHungry { password } => try_is_hungry(deps, password, env),
+        QueryMsg::GetStatus { password } => try_get_status(deps, password),
     }
 }
 
-pub fn permit_queries(
-    deps: Deps,
-    env: Env,
-    permit: Permit,
-    query: QueryWithPermit,
-) -> StdResult<Binary> {
-    let account = secret_toolkit::permit::validate(
-        deps,
-        "revoked_permits",
-        &permit,
-        env.contract.address.to_string(),
-        None,
-    )?;
-
-    // permit validated, process query
-    match query {
-        QueryWithPermit::GetSecret { } => query_secret(deps, &env.block, &deps.api.addr_canonicalize(&account)?)
+fn check_password(deps: Deps, password: String) -> StdResult<()> {
+    let stored_password = PASSWORD.load(deps.storage)?;
+    if stored_password != password {
+        return Err(StdError::generic_err("Incorrect Password"));
     }
+    Ok(())
 }
 
-fn query_auction_info(deps: Deps) -> StdResult<Binary> {
-    let started;
-    let mut minimum_bid = None;
-    let mut end_time = None;
-    if !AUCTION_STARTED.load(deps.storage)? {
-        started = false;
-    } else {
-        started = true;
-        let auction = AUCTION.load(deps.storage)?;
-        minimum_bid = Some(Uint128::from(auction.minimum_bid));
-        end_time = Some(Uint64::from(auction.end_time.seconds()));
-    }
-    Ok(to_binary(&QueryAnswer::AuctionInfo { started, minimum_bid, end_time })?)
-}
-
-fn query_secret(deps: Deps, block: &BlockInfo, account: &CanonicalAddr) -> StdResult<Binary> {
-    verify_auction_finished(deps.storage, block)?;
-    let highest_bid = HIGHEST_BID.load(deps.storage)?;
-
-    let auction = AUCTION.load(deps.storage)?;
-    if highest_bid.amount < auction.minimum_bid {
-        return Err(StdError::generic_err("Unauthorized"));
-    }
-    
-    if highest_bid.bidder != *account {
-        return Err(StdError::generic_err("Only the winner of the auction can see the secret"));
-    }
-
+pub fn try_is_hungry(deps: Deps, password: String, env: Env) -> StdResult<Binary> {
+    check_password(deps, password)?;
+    let pet = PET.load(deps.storage)?;
+    let hunger = pet.hunger_level;
+    let last_action_block = pet.last_action_block;
+    let blocks_passed = env.block.height - last_action_block;
+    let hunger = (hunger + blocks_passed as u8).min(10);
     Ok(
-        to_binary(&QueryAnswer::GetSecret { secret: auction.secret })?
+        to_binary(&QueryAnswer::IsHungry { is_hungry: (hunger >= 7) })?
     )
 }
 
+pub fn try_get_status(deps: Deps, password: String) -> StdResult<Binary> {
+    check_password(deps, password)?;
+    let pet = PET.load(deps.storage)?;
+    Ok(
+        to_binary(&QueryAnswer::GetStatus {
+            name: pet.name,
+            hunger_level: pet.hunger_level,
+            happiness_level: pet.happiness_level,
+            energy_level: pet.energy_level,
+        })?
+    )
+}
 
 
 // #[cfg(test)]
